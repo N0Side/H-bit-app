c08d9076827260da03efea8fba159321c854e658
Fé Van de Vyver <s112783@ap.be>
2022-01-19 06:16:25 +0100

    fixed clock display

--- app/index.js
+++ app/index.js
@@ -6,7 +6,15 @@ clock.granularity = 'minutes'; // seconds, minutes, hours
 const clockLabel = document.getElementById('clock-label');
 
 clock.addEventListener('tick', (evt) => {
-  clockLabel.text = evt.date.getHours() + ':' + evt.date.getMinutes();
+  let clockhours = evt.date.getHours();
+  let clockminutes = evt.date.getMinutes();
+  if (clockhours < 10) {
+    clockhours = '0' + clockhours;
+  }
+  if (clockminutes < 10) {
+    clockminutes = '0' + clockminutes;
+  }
+  clockLabel.text = clockhours + ':' + clockminutes;
 });
 
 const datum = document.getElementById('datum');
@@ -17,3 +25,24 @@ clock.addEventListener('tick', (evt) => {
 
 const date1 = new Date();
 console.log(date1.getUTCDay());
+
+/*
+let u=0;
+if (u>0) {
+  console.log(item.days.value(id[u]));
+  u++;
+}
+else {
+  console.log("oeche");
+}
+});
+
+const datum = document.getElementById('datum');
+
+clock.addEventListener('tick', (evt) => {
+datum.text = evt.date;
+});
+
+const date1= new Date;
+console.log(date1.getUTCDay());
+*/
