d6bd945c407a2ce264a6adc65ab7cdb5838c78c0
Fé Van de Vyver <s112783@ap.be>
2022-01-19 11:18:44 +0100

    cleanup

--- companion/index.js
+++ companion/index.js
@@ -1,9 +1,6 @@
 import * as cbor from 'cbor';
 import { outbox } from 'file-transfer';
 import { settingsStorage } from 'settings';
-import * as messaging from 'messaging';
-import { geolocation } from 'geolocation';
-import { API_KEY } from './keys.example';
 
 /* Settings */
 function sendSettings() {
@@ -27,71 +24,3 @@ function sendSettings() {
 }
 
 settingsStorage.addEventListener('change', sendSettings);
-
-/* Sending short messages */
-function sendShortMessage() {
-  const data = {
-    companionTimestamp: new Date().getTime(),
-  };
-
-  if (messaging.peerSocket.readyState === messaging.peerSocket.OPEN) {
-    messaging.peerSocket.send(data);
-  }
-}
-
-messaging.peerSocket.addEventListener('open', () => {
-  setInterval(sendShortMessage, 10000);
-});
-
-messaging.peerSocket.addEventListener('error', (err) => {
-  console.error(`Connection error: ${err.code} - ${err.message}`);
-});
-
-/* API Fetch */
-async function fetchLocationName(coords) {
-  const url = `https://api.mapbox.com/geocoding/v5/mapbox.places/${coords.longitude},${coords.latitude}.json?access_token=${API_KEY}`;
-
-  const response = await fetch(url);
-  const json = await response.json();
-
-  let location = '';
-  json.features.forEach((feature) => {
-    if (
-      !location &&
-      (feature.place_type[0] === 'locality' ||
-        feature.place_type[0] === 'place')
-    ) {
-      location = feature.text;
-    }
-  });
-
-  outbox
-    .enqueue('location.cbor', cbor.encode({ location }))
-    .then(() => console.log(location + ' as location sent'))
-    .catch((error) => console.log(`send error: ${error}`));
-}
-
-/* Location functions */
-function locationSuccess(location) {
-  fetchLocationName(location.coords);
-}
-
-function locationError(error) {
-  console.log(`Error: ${error.code}`, `Message: ${error.message}`);
-  // Handle location error (send message to device to show error)
-}
-
-/* Handle messages coming from device */
-function processMessaging(evt) {
-  console.log(evt.data);
-  switch (evt.data.command) {
-    case 'location':
-      geolocation.getCurrentPosition(locationSuccess, locationError);
-      break;
-    default:
-      //
-      break;
-  }
-}
-
-messaging.peerSocket.addEventListener('message', processMessaging);
